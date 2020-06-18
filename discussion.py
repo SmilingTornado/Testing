7edff97991a347837ffdb50e8536b36719b3aa5d
Worawat <KimparaW@cardiff.ac.uk>
2020-06-18 09:35:27 +0700

    discussion modification

--- discussion.py
+++ discussion.py
@@ -1,15 +1,16 @@
-def big_diff(list):
-    if len(list)!=1:
-        current = list
-        list = list[1:]
-        previous = big_diff(list)[0]
-        if max(i for i in previous)-min(i for i in previous) > \
+def big_diff(lists):
+    if len(lists) != 1:
+        current = lists
+        lists = lists[1:]
+        previous = big_diff(lists)[0]
+        if max(i for i in previous) - min(i for i in previous) > \
                 max(i for i in current[0]) - min(i for i in current[0]):
             return [previous]
         else:
             return [current[0]]
     else:
-        return [list[0]]
+        return lists
 
-my_parameter = [[3,9],[4,8],[5,6],[7,8],[6,8],[6,9]]
+
+my_parameter = [[3, 9], [4, 8], [5, 6], [7, 8], [6, 8], [6, 9]]
 print(big_diff(my_parameter))
