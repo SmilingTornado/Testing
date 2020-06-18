ff0e7ad244dd06d04374ae6fd519a1c3d977dad9
Worawat <KimparaW@cardiff.ac.uk>
2020-06-18 09:39:29 +0700

    discussion modification

--- discussion.py
+++ discussion.py
@@ -1,13 +1,13 @@
 def big_diff(lists):
     if len(lists) != 1:
-        current = lists
+        current = lists[0]
         lists = lists[1:]
         previous = big_diff(lists)[0]
         if max(i for i in previous) - min(i for i in previous) > \
-                max(i for i in current[0]) - min(i for i in current[0]):
+                max(i for i in current) - min(i for i in current):
             return [previous]
         else:
-            return [current[0]]
+            return [current]
     else:
         return lists
 
